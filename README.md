cba3bff0cafaf65c46121323e15e792f0cd4df8e
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-03-07 10:58:33 -0500

    Update Mandatory Intents with Lambda setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# find-linkNYC
+# find-link-nyc
 An Amazon Echo Skill that helps you find a LinkNYC kiosk
+
+## TODO
+
+- [ ] Find Nearest Intent to Locate nearest kiosk
+
+## License
+
+MIT
--- help-intent-function.js
+++ help-intent-function.js
@@ -6,12 +6,10 @@ module.exports = HelpIntentFunction
 // param (out): request: allows the user to change the response by Alexa
 
 function HelpIntentFunction (intent, session, response) {
-  var begin = 'This is a skill that allows you to Edit your Google Drive Files in your Respective Google Drive Account. '
-  var useListFiles = 'In order to list your files from your drive, please say Alexa, ask edit docs list my files please. '
-  var useCreateFiles = 'In order to create a file, simply say Alexa, ask edit docs to create a document called Sample. '
-  var useEditFiles = 'In order to add text to the end of a document, say Alexa, ask edit docs to update Sample with the following sample text. '
+  var begin = 'This is a skill that allows you to find a link NYC kiosk. '
+  var useFindNearest = 'In order to find your nearest link NYC, please say Alexa, ask linkNYC to give me the nearest link kiosk. '
   var question = 'What would you like to do ?'
-  var output = begin + useListFiles + useCreateFiles + useEditFiles + question
+  var output = begin + useFindNearest + question
   response.ask(output)
   return
 }
